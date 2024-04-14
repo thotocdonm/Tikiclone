39f279d143a0b5e138e4e7b18beed4d349994eca
thotocdonm <minhson411@gmail.com>
2024-04-14 14:04:03 +0700

    initialize project

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <>
+            login Page
+        </>
+    )
+}
+
+export default LoginPage
